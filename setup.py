db95a6705004368b3f02abf6d7c55d340568de3a
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2018-01-18 22:56:10 +0900

    requirements

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(name='Distutils',
       url='https://github.com/y-ich/go_translator',
       license='MIT',
       packages=['go_translator'],
+      install_requires=requires,
       classifiers=[
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 2.7',
